cda52bcae7d38ffbf786414f5058affeb06be557
Radoslav Kirilov <rkirilow@gmail.com>
2017-11-08 14:09:53 +0200

    add ">" as escape charater for ATTRIBUTE_NODE and TEXT_NODE encoding

--- test/dom/serializer.js
+++ test/dom/serializer.js
@@ -5,7 +5,23 @@ wows.describe('XML Serializer').addBatch({
   'text node containing "]]>"': function() {
     var doc = new DOMParser().parseFromString('<test/>', 'text/xml');
     doc.documentElement.appendChild(doc.createTextNode('hello ]]> there'));
-    console.assert(doc.documentElement.firstChild.toString() == 'hello ]]> there',doc.documentElement.firstChild.toString());
+    console.assert(doc.documentElement.firstChild.toString() == 'hello ]]&gt; there',doc.documentElement.firstChild.toString());
+  },
+  'text node containing "<>&"': function() {
+      var doc = new DOMParser().parseFromString('<test/>', 'text/xml');
+      doc.documentElement.appendChild(doc.createTextNode('<hello> <script>how are</script> you and &amp; your friend "Tomi"'));
+      console.assert(
+          doc.documentElement.firstChild.toString() == '&lt;hello&gt; &lt;script&gt;how are&lt;/script&gt; you and &amp;amp; your friend "Tomi"',
+          doc.documentElement.firstChild.toString()
+      );
+  },
+  'attribute node containing "<>&""': function() {
+      var doc = new DOMParser().parseFromString('<test/>', 'text/xml');
+      doc.documentElement.appendChild(doc.createTextNode('<hello> <script>how are</script> you and &amp; your friend "Tomi"'));
+      console.assert(
+          doc.documentElement.firstChild.toString() == '&lt;hello&gt; &lt;script&gt;how are&lt;/script&gt; you and &amp;amp; your friend "Tomi"',
+          doc.documentElement.firstChild.toString()
+      );
   },
   '<script> element with no children': function() {
     var doc = new DOMParser({xmlns:{xmlns:'http://www.w3.org/1999/xhtml'}}).parseFromString('<html2><script></script></html2>', 'text/html');
